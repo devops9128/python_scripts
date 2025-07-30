0dc121a81808dc395c89d7aef6d73ea138df1869
ubuntu_hp <devops9128@gmail.com>
2025-07-30 09:51:09 +0800

    Resolve the process will continue execute when receive an error, adding second layer of try & except in while loop

--- web_ping.py
+++ web_ping.py
@@ -2,7 +2,8 @@ from time import sleep
 import requests
 import time
 from datetime import datetime
-
+from requests.exceptions import RequestException
+from urllib3.exceptions import NameResolutionError
 
 """
     核心要点
@@ -25,56 +26,67 @@ from datetime import datetime
       
         def func(easy=None, middle=None, hard): ❌
         def func(hard, esy=None, middle=None):  ✅
+        
+    * 外层 try 包裹 while	整个 while 被中断，直接进入 except	❌ 不会继续 [ 就像“公交车一旦爆胎，整条线路停运”。 ]
+    * while 内部 try	本次异常被捕获，下一轮 while 继续	✅ 会继续 [ 就像“公交车爆胎后修好，再继续下一趟”。 ]
 """
 
 odoo_web = "https://production-kul.unitedcaps.com"
 google_web = "https://www.google.com"
 
-
+# Define a function to output a file record
 def log_timeout(url: str = None, status: str = None, error: str = None) -> None:
     timestamp = datetime.now().strftime("%d-%m-%Y - %H:%M:%S")
     with open('timeout.txt', mode='a', encoding='UTF-8') as f:
         f.write(f"{timestamp} {url}， Return code: {status} failed access\n")
 
+# Print the head line when execute the process
+print(f"开始测试 {odoo_web} 时间 : {datetime.now().strftime("%d-%m-%Y - %H:%M:%S")}")
+with open('.\\timeout.txt', mode='a', encoding='UTF-8') as f:
+    f.write(f"开始测试 {odoo_web} 时间 : {datetime.now().strftime("%d-%m-%Y - %H:%M:%S")}\n")
 
+# First layer try
 try:
-
-    print(f"开始测试 {odoo_web} 时间 : {datetime.now().strftime("%d-%m-%Y - %H:%M:%S")}")
-    with open('.\\timeout.txt', mode='a', encoding='UTF-8') as f:
-        f.write(f"开始测试 {odoo_web} 时间 : {datetime.now().strftime("%d-%m-%Y - %H:%M:%S")}\n")
-
+    # Execute while loop
     while True:
+        # Second layer try
+        try:
 
-        # Odoo连接测试
-        odoo_respone = requests.get(odoo_web, timeout=5)
-        odoo_status = odoo_respone.status_code
+            # Odoo link connection test
+            odoo_respone = requests.get(odoo_web, timeout=5)
+            odoo_status = odoo_respone.status_code
 
-        if odoo_status != 200:
-            log_timeout(url=odoo_web, status=odoo_status, error=None)
-        else:
-            print(
-                f"Return code: {odoo_status} {odoo_web} {datetime.now().strftime("%d-%m-%Y (%H:%M:%S)")} access normal🍀"
-            )
+            if odoo_status != 200:
+                log_timeout(url=odoo_web, status=odoo_status, error=None)
+            else:
+                print(
+                    f"Return code: {odoo_status} {odoo_web} {datetime.now().strftime("%d-%m-%Y (%H:%M:%S)")} access normal🍀"
+                )
 
-        # Google连接测试
-        google_respone = requests.get(google_web, timeout=5)
-        google_status = google_respone.status_code
+        except RequestException as e:
+            print(f"⚠️ Odoo请求错误: {e}")
+            log_timeout(url=odoo_web, status=None, error=str(e))
 
-        if google_status != 200:
-            log_timeout(url=google_web, status=google_status, error=None)
-        else:
-            print(
-                f"Return code: {google_status} {google_web} {datetime.now().strftime("%d-%m-%Y (%H:%M:%S)")} access normal🍀"
-            )
+        try:
 
+            # Google link connection test
+            google_respone = requests.get(google_web, timeout=5)
+            google_status = google_respone.status_code
+
+            if google_status != 200:
+                log_timeout(url=google_web, status=google_status, error=None)
+            else:
+                print(
+                    f"Return code: {google_status} {google_web} {datetime.now().strftime("%d-%m-%Y (%H:%M:%S)")} access normal🍀"
+                )
+
+        except RequestException as e:
+            print(f"⚠️ Google请求错误: {e}")
+            log_timeout(url=google_web, status=None, error=str(e))
+
+        # Continue execution after 10 seconds
         sleep(10)
 
+# Exit the process when detect ctrl + c
 except KeyboardInterrupt:
     print("\n检测到 Ctrl+C，程序即将退出。")
-
-except requests.exceptions.Timeout as e:
-    print("Timeout request, try later...")
-    log_timeout(url=None, status=None, error=e)
-
-except Exception as ee:
-    print(f"接收到错误{ee}")
